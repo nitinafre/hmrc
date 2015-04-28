f7344bddef363124f44b0ce8d1fbe2f44d70eb97
Nitin Afre <nitinafre@gmail.com>
2015-04-28 10:02:10 +0100

    Some code cleanup and documentation

--- shopping/src/main/java/com/hmrc/service/WebCheckoutService.java
+++ shopping/src/main/java/com/hmrc/service/WebCheckoutService.java
@@ -18,7 +18,7 @@ public class WebCheckoutService implements CheckoutService {
 		if (MapUtils.isEmpty(products)) {
 			throw new IllegalArgumentException("Cannot checkout with empty basket");
 		}
-		assert null != products;
+
 		Double checkoutAmount = 0.0;
 
 		for (Product product : products.keySet()) {
@@ -28,15 +28,15 @@ public class WebCheckoutService implements CheckoutService {
 		return checkoutAmount;
 	}
 
-	/* In order to avoid complex rule engine code, this simple approach is taken. It can be improvised using various rule engines tool (as it is generally very short timed and changed frequently) */
+	/* In order to avoid complex rule engine code, this simple approach is taken. It can be improvised using various rule engines tool (as it is generally very short timed and changed frequently)
+	 * Later the return type can be changed to float to include offer like buy 1 get second half price.
+	 *  */
 	private int checkOffer(Integer productId, Integer count) {
-		if (productId.equals(1)) { // Consider this as an Apple
+		if (productId.equals(1)) { // Consider this as an Apple (buy 1 get 1 free)
 			return (count / 2) + (count % 2);
 		}
-		if (productId.equals(2)) { // Consider this as an Orange
-			if (count > 2) {
-				return ((count / 3) * 2) + (count % 3);
-			}
+		if (productId.equals(2)) { // Consider this as an Orange (buy 2 get 1 free or 3 for price of 2)
+			return ((count / 3) * 2) + (count % 3);
 		}
 		return count;
 	}
